c6a94cc4e811bc543f66f60fd992df4a777aea4d
R KARIM MALIK <35759105+karimmalik@users.noreply.github.com>
2021-03-19 10:09:01 +0700

    Update Constants.java

--- expense-tracker-api-master/src/main/java/com/karim/test/Constants.java
+++ expense-tracker-api-master/src/main/java/com/karim/test/Constants.java
@@ -3,7 +3,7 @@ package com.karim.test;
 public class Constants {
 
     public static final String API_SECRET_KEY = "expensetrackerapikey";
-    public static final String PRODUCT = "Product";
+    public static final String PRODUCT = "product";
     public static final String PACKAGEACTIVATION = "packageActivation";
     public static final long TOKEN_VALIDITY = 2 * 60 * 60 * 12000;
 
